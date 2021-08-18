f0b486bf9b3f7418e768b69abf859b6730206612
Shuo Zhang <shuzha@microsoft.com>
2021-08-18 14:23:15 -0700

    Add name and email to ProfileScreen.

--- AppCode/app/screens/ProfileScreen.js
+++ AppCode/app/screens/ProfileScreen.js
@@ -1,50 +1,58 @@
 import React from 'react';
-import { StyleSheet, ScrollView } from 'react-native';
+import { View, StyleSheet, ScrollView, Image, Text } from 'react-native';
 
 import Screen from '../components/Screen';
 import colors from '../config/colors';
+import useAuth from '../auth/useAuth';
 import routes from "../navigation/routes";
 import AppText from "../components/AppText";
 import BackButton from "../components/BackButton";
 
 
 function ProfileScreen({ navigation }) {
+    const { user } = useAuth();
+
     return (
         <Screen style={styles.screen}>
             <BackButton onPress={() => navigation.navigate(routes.SETTINGS)} />
-            <AppText style={styles.screenTitle}>Profile</AppText>
+            <View style={styles.accountInfo}>       
+                <Image style={styles.portrait} source={require('../assets/portrait_placeholder.png')} />
+                <Text style={styles.title}>{user.name}</Text>
+                <Text style={styles.subTitle}>{user.email}</Text>
+            </View>  
             <ScrollView>
-            {/* TODO: Get actual account details here */}
-            <AppText style={styles.detailsText}>
-                Account type: Student
-            </AppText>
+                {/* TODO: Get actual account details here */}
             </ScrollView>
         </Screen>
     );
 }
 
 const styles = StyleSheet.create({
+    accountInfo: {
+        marginVertical: 30,
+        alignItems: 'center',
+    },
     container: {
         marginTop: 20,
+    },
+    portrait: {
+        width: 100,
+        height: 100,
+        borderRadius: 50,
     },
     screen: {
         backgroundColor: colors.white,
     },
-    screenTitle: {
-        fontSize: 23,
-        fontWeight: 'bold',
-        color: colors.dark,
-        alignSelf: 'center',
-        marginTop: 25,
-        marginBottom: 25,
+    subTitle: {
+        fontSize: 15,
+        paddingVertical: 10,
+        color: colors.grey,
     },
-    detailsText: {
-        fontSize: 14,
-        fontWeight: '500',
-        color: colors.dark,
-        alignSelf: 'auto',
-        marginBottom: 25,
-        marginLeft: 25,
+    title: {
+        fontSize: 20,
+        paddingVertical: 10,
+        color: colors.black,
+        fontWeight: 'bold',
     },
 })
 
