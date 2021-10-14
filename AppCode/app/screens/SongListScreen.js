58f094241a0efef0439a66e2ef003edfda7d5ecd
Tingting Hu <tingh@microsoft.com>
2021-10-13 23:12:09 -0700

    Stop music playing when leaving page

--- AppCode/app/screens/SongListScreen.js
+++ AppCode/app/screens/SongListScreen.js
@@ -10,7 +10,7 @@ import SongListItem from "../components/SongListItem";
 import getLessons from '../api/lessons';
 import { play, pause, resume, playNext } from '../media_control/audioController';
 
-function SongListScreen(props) {
+function SongListScreen({ navigation }) {
 
     const [songs, setSongs] = useState([]);
     const [playbackObj, setPlaybackObj] = useState(null);
@@ -22,14 +22,27 @@ function SongListScreen(props) {
     useEffect(() => {
         let mounted = true;
         getLessons()
-        .then(response => {
-            if(mounted) {
-            let songList = response.data[0].Songs
-            setSongs(songList)
-            }
-        })
+            .then(response => {
+                if (mounted) {
+                    let songList = response.data[0].Songs
+                    setSongs(songList)
+                }
+            })
         return () => mounted = false;
     }, []);
+
+    useEffect(() => {
+        const unsubscribe = navigation.addListener('blur', async () => {
+
+            setIsPlaying(false);
+            if (soundObj.isLoaded && soundObj.isPlaying) {
+                const status = await pause(playbackObj);
+                setSoundObj(status);
+            }
+        });
+
+        return unsubscribe;
+    }, [navigation, soundObj, playbackObj]);
 
     const handleAudioPress = async (songItem) => {
         // playing audio for the first time.
