32645f220719a5cea12a91124ea31b00f4efb035
Lin Sung Chiang <linsungc@outlook.com>
2021-10-30 00:19:11 -0700

    Lesson & Activity Status code refactoring and consolidation

---
+++ AppCode/app/data/lesson/lessoncontext.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const LessonContext = React.createContext();
+
+export default LessonContext;
--- AppCode/app/screens/LessonDetailsScreen.js
+++ AppCode/app/screens/LessonDetailsScreen.js
@@ -12,23 +12,28 @@ import Screen from "../components/Screen";
 import Icon from "../components/Icon";
 import uistrings from "../config/uistrings";
 import useAuth from "../auth/useAuth";
-import { getActivityStatus } from "../api/status";
+import useLesson from "../data/lesson/lessondata";
 import LessonCompletion from "../components/LessonCompletion";
 
 function LessonDetailsScreen({ navigation, route }) {
   const lesson = route.params;
   const { logOut } = useAuth();
+  const {
+    fetchStatusData,
+    isLessonCompleted,
+    getStatus,
+    onPlayStateChanged,
+    playStateChanged,
+  } = useLesson();
 
   // Need to pause the video when navigate away to a new screen
   const player = useRef(null);
-  const [activityStatus, setActivityStatus] = useState(null);
-  const [playStateChanged, setPlayStateChanged] = useState(false);
   const [statusRefreshed, setStatusRefreshed] = useState(false);
 
   useEffect(() => {
     const blur = navigation.addListener("blur", () => {
       player?.current?.pauseAsync();
-      setPlayStateChanged(false);
+      onPlayStateChanged(false);
     });
 
     return blur;
@@ -41,9 +46,8 @@ function LessonDetailsScreen({ navigation, route }) {
 
       // Workaround to delay GET call so the POST call happens first
       setTimeout(() => {
-        getActivityStatus().then((response) => {
+        fetchStatusData().then((response) => {
           setStatusRefreshed(true);
-
           if (response == null) {
             const reloginAlert = () => {
               Alert.alert(uistrings.Messages, uistrings.RequireRelogin, [
@@ -52,9 +56,6 @@ function LessonDetailsScreen({ navigation, route }) {
             };
 
             reloginAlert();
-          } else {
-            const data = response.data;
-            setActivityStatus(data);
           }
         });
       }, 300);
@@ -80,78 +81,10 @@ function LessonDetailsScreen({ navigation, route }) {
     }
   };
 
-  const isLessonCompleted = (lesson) => {
-    const percentage = GetLessonStatus(lesson);
-
-    if (percentage == 100) {
-      return true;
-    } else {
-      return false;
-    }
-  };
-
-  const GetLessonStatus = (lesson) => {
-    try {
-      let lessonStatus = 0;
-
-      if (activityStatus == undefined) return 0;
-
-      const activity = activityStatus.filter(
-        (item) => item.LessonId == lesson._id
-      );
-
-      const completionStatus = activity.map((item) => {
-        return item.CompletionStatus;
-      });
-
-      const completedStatus = completionStatus.reduce(
-        (totalCompleted, activityStatus) => {
-          // Increment if an activity was completed (10 == 100%)
-          if (activityStatus == 10) totalCompleted = totalCompleted + 1;
-
-          return totalCompleted;
-        },
-        0
-      );
-
-      let acitivityCount = lesson.Activities.length;
-      acitivityCount = acitivityCount == undefined ? 0 : acitivityCount;
-
-      lessonStatus = Math.round((completedStatus / acitivityCount) * 100);
-
-      if (isNaN(lessonStatus)) lessonStatus = 0;
-
-      return lessonStatus;
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const getPlayState = (lessonId, activityId) => {
-    try {
-      let completionStatus = 0;
-
-      if (activityStatus == undefined) return;
-
-      const activity = activityStatus.filter(
-        (item) => item.ActivityId == activityId && item.LessonId == lessonId
-      );
-
-      completionStatus =
-        activity[0]?.CompletionStatus == null
-          ? 0
-          : activity[0].CompletionStatus;
-
-      return completionStatus;
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   const lessonLength = Math.round(lesson.IntroVideo.LengthInSeconds / 60);
 
   const renderItem = (item) => {
-    const state = getPlayState(lesson._id, item._id);
+    const state = getStatus(lesson._id, item._id);
 
     return (
       <ActivityListItem
@@ -167,9 +100,6 @@ function LessonDetailsScreen({ navigation, route }) {
             activityId: item._id,
             activityVideo: item.Videos[0],
             activityPlayState: state,
-            onPlayStateChange: (updated) => {
-              setPlayStateChanged(updated);
-            },
           })
         }
       />
@@ -210,7 +140,9 @@ function LessonDetailsScreen({ navigation, route }) {
         </View>
         <ListItemSeparator />
         <View style={styles.activityContainer}>
-          <AppText style={styles.activitySectionTitle}>{uistrings.Activities} ({lesson.Activities.length})</AppText>
+          <AppText style={styles.activitySectionTitle}>
+            {uistrings.Activities} ({lesson.Activities.length})
+          </AppText>
           <SafeAreaView>
             <FlatList
               data={lesson.Activities}
