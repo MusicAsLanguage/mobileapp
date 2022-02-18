1c772b572064f24fabecf8430be9d632ffff2116
Lin Sung Chiang <linsungc@outlook.com>
2022-02-17 22:20:43 -0800

    Add pull to refresh function to ActivityScreen

--- AppCode/app/screens/ActivityDetailsScreen.js
+++ AppCode/app/screens/ActivityDetailsScreen.js
@@ -1,7 +1,19 @@
-import React, { useEffect, useRef, useState } from "react";
-import { Platform, StyleSheet, StatusBar, View } from "react-native";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import {
+  Platform,
+  RefreshControl,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  StatusBar,
+  View,
+} from "react-native";
 import { Video } from "expo-av";
 import ActivityCompletion from "../components/ActivityCompletion";
+import colors from "../config/colors";
+import Icon from "../components/Icon";
+import ErrorMessage from "../components/forms/ErrorMessage";
+import uistrings from "../config/uistrings";
 
 function ActivityScreen({ navigation, route }) {
   const { lessonId, activityId, activityVideo, activityPlayState } =
@@ -18,25 +30,63 @@ function ActivityScreen({ navigation, route }) {
 
   const { onPlayStateChanged, updateStatusData } = useLesson();
 
-  useEffect(() => {
-    // Workaround to hide the status bar & the tab bar to make video full screen
-    const parent = navigation.dangerouslyGetParent();
-    parent.setOptions({
-      tabBarVisible: false,
-    });
+  // Add ability to refresh the data
+  const [refreshing, setRefreshing] = useState(false);
+  const [videoUri, setVideoUri] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
 
-    if (Platform.OS === "ios") {
-      navigation.setOptions({
-        headerBackTitle: " ",
+  const wait = (timeout) => {
+    return new Promise((resolve) => setTimeout(resolve, timeout));
+  };
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+
+    // clear the error message displayed on video screen
+    setErrorMsg("");
+
+    wait(2000).then(() => {
+      // refresh the data
+
+      // set "refresh" as signal so Video onError will ignore it
+      setVideoUri("refresh");
+
+      // workaround as the video does not load the second time due to videoUri not getting in time
+      wait(500).then(() => {
+        setVideoUri(activityVideo.Url);
       });
+
+      setRefreshing(false);
+    });
+  }, []);
+
+  useEffect(() => {
+    let mounted = true;
+
+    if (mounted) {
+      // Workaround to hide the status bar & the tab bar to make video full screen
+      const parent = navigation.dangerouslyGetParent();
+      parent.setOptions({
+        tabBarVisible: false,
+      });
+
+      if (Platform.OS === "ios") {
+        navigation.setOptions({
+          headerBackTitle: " ",
+        });
+      }
+
+      StatusBar.setHidden(true, "none");
+
+      setVideoUri(activityVideo.Url);
     }
 
-    StatusBar.setHidden(true, "none");
-
-    return () =>
+    return () => {
       parent.setOptions({
         tabBarVisible: true,
       });
+      mounted = false;
+    };
   }, []); // Run only once at mount
 
   useEffect(() => {
@@ -76,6 +126,7 @@ function ActivityScreen({ navigation, route }) {
   };
 
   const onLoad = async (playbackStatus) => {
+    setErrorMsg("");
     const durationMillis = playbackStatus.durationMillis;
     durationRef.current = durationMillis;
     setDuration(durationMillis);
@@ -88,10 +139,17 @@ function ActivityScreen({ navigation, route }) {
   };
 
   const onError = async (err) => {
+    // videoUri == "refreshing" is just to force the state refresh so ignore the error
+    if (videoUri == "refresh") return;
+
     console.log(
       //"Did you notice problem loading the video? Pull screen to retry"
-      err
+      err,
+      "-",
+      videoUri
     );
+
+    setErrorMsg(uistrings.VideoWasNotLoadedPullToRetry);
   };
 
   const onPlaybackStatusUpdate = async (playbackStatus) => {
@@ -122,21 +180,54 @@ function ActivityScreen({ navigation, route }) {
     }
   };
 
+  const showErrorMessage = () => {
+    if (errorMsg != "") {
+      return (
+        <View style={styles.errorMsgContainer}>
+          <ErrorMessage
+            error={errorMsg}
+            visible={true}
+            style={styles.errorMsg}
+          />
+          <Icon
+            name="arrow-down-circle"
+            iconColor={colors.white}
+            backgroudColor="transparent"
+            size={160}
+            style={styles.musicnote}
+          />
+        </View>
+      );
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Video
-        source={{ uri: activityVideo.Url }}
-        ref={player}
-        shouldPlay={true}
-        resizeMode="cover"
-        useNativeControls
-        onLoad={onLoad}
-        onError={onError}
-        onPlaybackStatusUpdate={onPlaybackStatusUpdate}
-        style={videoFinished ? styles.videoFaded : styles.video}
-      />
-      {showEndState()}
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView
+        contentContainerStyle={styles.scrollView}
+        refreshControl={
+          <RefreshControl
+            tintColor={colors.white}
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+          />
+        }
+      >
+        <Video
+          source={{ uri: videoUri }}
+          ref={player}
+          shouldPlay={false}
+          resizeMode="cover"
+          useNativeControls
+          onLoad={onLoad}
+          onError={onError}
+          onPlaybackStatusUpdate={onPlaybackStatusUpdate}
+          style={videoFinished ? styles.videoFaded : styles.video}
+        />
+        {showErrorMessage()}
+        {showEndState()}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -145,14 +236,31 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "black",
   },
+  scrollView: {
+    flex: 1,
+  },
   video: {
+    marginTop: 35,
     width: "100%",
-    height: "100%",
+    height: "90%",
   },
   videoFaded: {
+    marginTop: 35,
     width: "100%",
-    height: "100%",
+    height: "90%",
     opacity: 0.5,
+  },
+  errorMsgContainer: {
+    position: "absolute",
+    flexDirection: "column",
+    marginTop: 250,
+    marginLeft: 70,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  errorMsg: {
+    color: colors.white,
+    textAlign: "center",
   },
 });
 
