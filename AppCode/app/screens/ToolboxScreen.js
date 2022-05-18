36d83a72a3bc828fa96ad821ebded9c3158e14e3
Shuo Zhang <shuzha@microsoft.com>
2022-05-18 00:16:59 -0700

    Filter song list by category.

--- AppCode/app/screens/ToolboxScreen.js
+++ AppCode/app/screens/ToolboxScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, FlatList } from 'react-native';
-import { Ionicons, FontAwesome5 } from '@expo/vector-icons';
+import { Ionicons, FontAwesome5, MaterialCommunityIcons } from '@expo/vector-icons';
 
 import Screen from '../components/Screen';
 import ListItem from '../components/ListItem';
@@ -18,8 +18,24 @@ function ToolboxScreen({navigation}) {
             <View style={styles.musicContainer}>
                 <View style={styles.buttonContainer}>
                     <GradientButton
-                        onPress={() => navigation.navigate(routes.SONG_LIST)}
+                        onPress={() => navigation.navigate(routes.SONG_LIST, uistrings.SongCategoryInstruction)}
                         colors={[colors.yellowgreen, colors.yellowgreen, colors.white]}
+                        text="Instruction"
+                        renderIcon={() => (
+                            <FontAwesome5
+                                name="paper-plane"
+                                size={50}
+                                color={colors.white}
+                                style={{ marginLeft: 10, marginRight: 20 }}
+                            />
+                        )}
+                    />
+                </View>
+
+                <View style={styles.buttonContainer}>
+                    <GradientButton
+                        onPress={() => navigation.navigate(routes.SONG_LIST, uistrings.SongCategoryBeginner)}
+                        colors={[colors.deepskyblue, colors.deepskyblue, colors.white]}
                         text="Beginner"
                         renderIcon={() => (
                             <FontAwesome5
@@ -34,8 +50,8 @@ function ToolboxScreen({navigation}) {
 
                 <View style={styles.buttonContainer}>
                     <GradientButton
-                        onPress={() => navigation.navigate(routes.SONG_LIST)}
-                        colors={[colors.deepskyblue, colors.deepskyblue, colors.white]}
+                        onPress={() => navigation.navigate(routes.SONG_LIST, uistrings.SongCategoryIntermediate)}
+                        colors={[colors.magenta, colors.magenta, colors.white]}
                         text="Intermediate"
                         renderIcon={() => (
                             <Ionicons
@@ -50,12 +66,28 @@ function ToolboxScreen({navigation}) {
 
                 <View style={styles.buttonContainer}>
                     <GradientButton
-                        onPress={() => navigation.navigate(routes.SONG_LIST)}
-                        colors={[colors.magenta, colors.magenta, colors.white]}
-                        text="Advanced"
+                        onPress={() => navigation.navigate(routes.SONG_LIST, uistrings.SongCategorySuperStar)}
+                        colors={[colors.yellowgreen, colors.yellowgreen, colors.white]}
+                        text="Super Star"
                         renderIcon={() => (
                             <Ionicons
                                 name="rocket-outline"
+                                size={50}
+                                color={colors.white}
+                                style={{ marginLeft: 10, marginRight: 20 }}
+                            />
+                        )}
+                    />
+                </View>
+
+                <View style={styles.buttonContainer}>
+                    <GradientButton
+                        onPress={() => navigation.navigate(routes.SONG_LIST, uistrings.SongCategoryLegend)}
+                        colors={[colors.deepskyblue, colors.deepskyblue, colors.white]}
+                        text="Legend"
+                        renderIcon={() => (
+                            <MaterialCommunityIcons
+                                name="medal-outline"
                                 size={50}
                                 color={colors.white}
                                 style={{ marginLeft: 10, marginRight: 20 }}
@@ -70,11 +102,11 @@ function ToolboxScreen({navigation}) {
 
 const styles = StyleSheet.create({
     buttonContainer: {
-        marginVertical: 20,
+        marginVertical: 15,
     },
     musicContainer: {
         flex: 1,
-        marginTop: 100,
+        marginTop: 50,
         marginHorizontal: 50,
         justifyContent: 'center',
     },
