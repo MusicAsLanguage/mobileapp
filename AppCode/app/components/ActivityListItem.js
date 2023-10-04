7b2e22704f730efb17f1791eb5a3096100a999fe
JaneShuoZhang <janezhangshuo@hotmail.com>
2023-10-04 12:11:19 -0700

    add scores to each activity

--- AppCode/app/components/ActivityListItem.js
+++ AppCode/app/components/ActivityListItem.js
@@ -9,16 +9,19 @@ import AppText from "./AppText";
 import ActivityStatus from "./ActivityStatus";
 import colors from "../config/colors";
 import Icon from "./Icon";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 function ActivityListItem({
   id,
   name,
   description,
   duration,
+  score,
   thumbnail,
   status,
   repeats,
   practiceMode,
+  progressColor,
   onPress,
 }) {
   const durationMin = Math.round(duration / 60);
@@ -45,6 +48,13 @@ function ActivityListItem({
       <View style={styles.detail}>
         {id && <AppText style={styles.nameIndex}>Activity{id}</AppText>}
         {description && <AppText style={styles.name}>{name}</AppText>}
+      </View>
+
+      <View style={styles.pointContainer} >
+        <View style={[styles.pointCircle, {backgroundColor: progressColor}]}>
+          <MaterialCommunityIcons name="music-note" color={colors.black} size={18}></MaterialCommunityIcons>
+          <AppText style={styles.point}>+{score}pt</AppText>
+        </View>
       </View>
 
     </TouchableOpacity>
@@ -88,6 +98,7 @@ const styles = StyleSheet.create({
   detail: {
     flexDirection: "column",
     marginHorizontal: 5,
+    flex: 1,
   },
   name: {
     fontSize: 16,
@@ -95,6 +106,25 @@ const styles = StyleSheet.create({
   nameIndex: {
     fontSize: 16,
     fontWeight: "bold",
+  },
+  point: {
+    fontSize: 16,
+    marginRight: 3,
+  },
+  pointCircle: {
+    height: 40,
+    width: 95,
+    borderRadius: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
+    marginRight: 10,
+  },
+  pointContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    flex: 1,
+    justifyContent: "flex-end",
   },
 });
 
